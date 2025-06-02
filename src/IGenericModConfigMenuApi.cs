78c4a6d2714b23fe5c2ad4807a45f7d25147e3ae
HaNamJa0803 <shfdkwnj987@gmail.com>
2025-06-03 05:33:32 +0900

    Add files via upload

---
+++ src/IGenericModConfigMenuApi.cs
@@ -0,0 +1,28 @@
+using MyChildCore.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace MyChildCore
+{
+    public interface IGenericModConfigMenuApi
+    {
+        void Register(object mod, Action reset, Action save, bool titleScreenOnly = false);
+        void AddSectionTitle(object mod, Func<string> text, Func<string> tooltip = null);
+        void AddDropdown(
+            object mod,
+            Func<string> name,
+            Func<string> tooltip,
+            Func<string> getValue,
+            Action<string> setValue,
+            Func<IEnumerable<string>> allowedValues,
+            Func<string, string> format = null
+        );
+        void AddBoolOption(
+            object mod,
+            Func<string> name,
+            Func<string> tooltip,
+            Func<bool> getValue,
+            Action<bool> setValue
+        );
+    }
+}
