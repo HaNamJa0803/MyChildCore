34fea56b11b0ebb54b64e3eaebc13f39efc9503c
HaNamJa0803 <shfdkwnj987@gmail.com>
2025-06-03 14:10:05 +0900

    Add files via upload

---
+++ src/ModConfig.cs
@@ -0,0 +1,9 @@
+namespace MyChildCore
+{
+    public class ModConfig
+    {
+        public bool EnableMod { get; set; } = true;
+        public string GirlHairStyle { get; set; } = "Basic";
+        public string PajamaType { get; set; } = "Pajama1";
+    }
+}
