fd1ed55013eb2474c4e7e3bf0567ab17982570ea
HaNamJa0803 <shfdkwnj987@gmail.com>
2025-06-03 12:32:52 +0900

    Add files via upload

---
+++ src/SpouseSetting.cs
@@ -0,0 +1,56 @@
+using System.Runtime.CompilerServices;
+
+namespace MyChildCore;
+
+public class SpouseSetting
+{
+	[field: CompilerGenerated]
+	public bool UseMaleOutfit
+	{
+		[CompilerGenerated]
+		get;
+		[CompilerGenerated]
+		set;
+	} = true;
+
+
+	[field: CompilerGenerated]
+	public bool UseFemaleOutfit
+	{
+		[CompilerGenerated]
+		get;
+		[CompilerGenerated]
+		set;
+	} = true;
+
+
+	[field: CompilerGenerated]
+	public string MaleOutfit
+	{
+		[CompilerGenerated]
+		get;
+		[CompilerGenerated]
+		set;
+	} = "";
+
+
+	[field: CompilerGenerated]
+	public string FemaleOutfit
+	{
+		[CompilerGenerated]
+		get;
+		[CompilerGenerated]
+		set;
+	} = "";
+
+
+	[field: CompilerGenerated]
+	public ToddlerGenderSetting ToddlerGenderSetting
+	{
+		[CompilerGenerated]
+		get;
+		[CompilerGenerated]
+		set;
+	} = new ToddlerGenderSetting();
+
+}
