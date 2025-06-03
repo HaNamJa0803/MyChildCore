df6cf128001f111708d61c0903d5086a4f9de73e
HaNamJa0803 <shfdkwnj987@gmail.com>
2025-06-03 14:56:01 +0900

    Delete src/ModConfig.cs

--- src/ModConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace MyChildCore
-{
-    public class ModConfig
-    {
-        public bool EnableMod { get; set; } = true;
-        public string GirlHairStyle { get; set; } = "Basic";
-        public string PajamaType { get; set; } = "Pajama1";
-    }
-}
